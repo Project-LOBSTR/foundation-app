a3e4bdbf776361c94623a20cac160736f4c82b48
RicardoBrito1938 <ricardo.jucrist@hotmail.com>
2023-12-23 18:00:21 +0000

    chore: eslint config

--- app/page.tsx
+++ app/page.tsx
@@ -1,53 +1,50 @@
-"use client";
-import Layout from "@/components/Layout";
-import NDK from "@nostr-dev-kit/ndk";
-import Image from "next/image";
-import { useRouter } from "next/navigation";
-import { useCallback, useEffect } from "react";
+'use client'
 
-type Button = {
-  id: string;
-  label: string;
-  button: string;
-  onClick: () => void;
-};
+import { Button } from '@/components/Button'
+import Layout from '@/components/Layout'
+import { useRouter } from 'next/navigation'
+
+type ButtonProps = {
+  id: string
+  label: string
+  buttonText: string
+  onClick: () => void
+}
 
 export default function Home() {
-  const router = useRouter();
+  const router = useRouter()
 
-  // TODO: which router method is more performant?
-  const buttons: Button[] = [
+  const buttons: ButtonProps[] = [
     {
-      id: "sign-up",
-      label: "New NOSTR user?",
-      button: "Create new account",
-      onClick: () => router.push("/sign-up"),
+      id: 'sign-up',
+      label: 'New NOSTR user?',
+      buttonText: 'Create new account',
+      onClick: () => router.push('/sign-up'),
     },
     {
-      id: "sign-in",
-      label: "Existing NOSTR user?",
-      button: "Sign in with NOSTR",
-      onClick: () => router.push("/sign-in"),
+      id: 'sign-in',
+      label: 'Existing NOSTR user?',
+      buttonText: 'Sign in with NOSTR',
+      onClick: () => router.push('/sign-in'),
     },
-  ];
+  ]
 
   return (
     <Layout heading="Welcome to LOBSTR">
-      <div className="flex flex-col gap-4">
-        {buttons.map((button: Button) => {
-          return (
-            <div key={button.id} className="flex flex-col items-center">
-              <p className="text-black">{button.label}</p>
-              <button
-                className="bg-primary text-white px-12 py-4 text-sm rounded-sm"
-                onClick={button.onClick}
-              >
-                {button.button}
-              </button>
-            </div>
-          );
-        })}
+      <div className="my-10 flex flex-col items-center gap-8">
+        {buttons.map(({ id, label, buttonText, onClick }) => (
+          <div key={id} className="text-center">
+            <p className="mb-2 text-sm font-medium text-zinc-500">{label}</p>
+            <Button
+              variant="primary"
+              onClick={onClick}
+              className="w-full sm:w-auto"
+            >
+              {buttonText}
+            </Button>
+          </div>
+        ))}
       </div>
     </Layout>
-  );
+  )
 }
--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,29 +1,28 @@
-import Image from "next/image";
-import { ReactNode } from "react";
+import Image from 'next/image'
+import { PropsWithChildren } from 'react'
 
 type Props = {
-  children: ReactNode;
-  heading?: string;
-};
+  heading?: string
+}
 
-const Layout = ({ heading, children }: Props) => {
+const Layout = ({ heading, children }: PropsWithChildren<Props>) => {
   return (
-    <main className="flex min-h-screen flex-col bg-white items-center gap-10 justify-center p-12">
-      <div className="items-center flex flex-col">
+    <main className="flex min-h-screen flex-col items-center justify-center gap-10 bg-white p-12">
+      <div className="flex flex-col items-center">
         <Image
-          src={"/lobstr-logo.png"}
+          src={'/lobstr-logo.png'}
           alt="Lobstrrr"
           width={500}
           height={500}
           priority
         />
         {heading && (
-          <h1 className="text-primary font-heading text-xl">{heading}</h1>
+          <h1 className="text-primary-500 font-heading text-xl">{heading}</h1>
         )}
       </div>
       {children}
     </main>
-  );
-};
+  )
+}
 
-export default Layout;
+export default Layout
