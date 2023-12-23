3bd7e82cd2f05ba4c1e70465eea68db309a53003
RicardoBrito1938 <ricardo.jucrist@hotmail.com>
2023-12-23 22:46:26 +0000

    test:button

--- __tests__/Home.test.tsx
+++ __tests__/Home.test.tsx
@@ -1,8 +1,8 @@
-import { render, screen } from "@testing-library/react";
-import Home from "@/app/page";
+import { render, screen } from '@testing-library/react'
+import Home from '@/app/page'
 
-it("The text Welcome to LOBSTR will display on the home page", () => {
-  render(<Home />);
+it('The text Welcome to LOBSTR will display on the home page', () => {
+  render(<Home />)
 
-  expect(screen.getByText("Welcome to LOBSTR")).toBeInTheDocument();
-});
+  expect(screen.getByText('Welcome to LOBSTR')).toBeInTheDocument()
+})
--- jest.config.ts
+++ jest.config.ts
@@ -1,19 +1,19 @@
-import type { Config } from "jest";
-import nextJest from "next/jest.js";
+import type { Config } from 'jest'
+import nextJest from 'next/jest.js'
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
-  dir: "./",
-});
+  dir: './',
+})
 
 // Add any custom config to be passed to Jest
 const config: Config = {
   // Add more setup options before each test is run
   //   coverageProvider: "v8",
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
-  testEnvironment: "jest-environment-jsdom",
-  preset: "ts-jest",
-};
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testEnvironment: 'jest-environment-jsdom',
+  preset: 'ts-jest',
+}
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-export default createJestConfig(config);
+export default createJestConfig(config)
