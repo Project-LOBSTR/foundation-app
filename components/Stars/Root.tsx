572fc3e5f8e86194806ed9db1c51e6fd8a369f0d
RicardoBrito1938 <ricardo.jucrist@hotmail.com>
2024-01-28 14:09:00 +0000

    chore: add tailwind merge for classes

--- components/Stars/Root.tsx
+++ components/Stars/Root.tsx
@@ -1,7 +1,11 @@
 import { ComponentProps } from 'react'
 
+import { twMerge } from 'tailwind-merge'
+
 type Props = ComponentProps<'div'>
 
-export const Root = ({ ...props }: Props) => {
-  return <div {...props} />
+export const Root = (props: Props) => {
+  return (
+    <div className={twMerge('flex', 'flex-row', props.className)} {...props} />
+  )
 }
