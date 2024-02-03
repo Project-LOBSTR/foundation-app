1ec8c16a71d0d391b6f6501411e6af18116be66b
mattwilson02 <mattwilsonbmx@gmail.com>
2024-02-03 12:18:53 +0000

    feat: make the app downloadable

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+})
 
-module.exports = nextConfig
+module.exports = withPWA({})
