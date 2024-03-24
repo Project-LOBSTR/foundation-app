fcb0ce7970a2fdb779cf9625a752742d688ceecb
mattwilson02 <mattwilsonbmx@gmail.com>
2024-03-24 11:56:29 +0000

    chore: logout

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,4 +1,18 @@
+'use client'
+import { useEffect } from 'react'
+
+import { useRouter } from 'next/navigation'
+
+import { useAppSelector } from '@/redux/store'
+
 const Profile = () => {
+  const router = useRouter()
+  const pubKey = useAppSelector(({ user }) => user.publickey)
+
+  useEffect(() => {
+    if (!pubKey) router.replace('/')
+  }, [pubKey, router])
+
   return (
     <div>
       <h1>Profile</h1>
--- constants/routes.ts
+++ constants/routes.ts
@@ -1,9 +1,7 @@
 export const routes = {
   chooseAppMode: '/choose-app-mode',
-
   emergencyContact: '/emergency-contact',
   personalDetails: '/personal-details',
-
   recoverSeedPhrase: '/recover-seed-phrase',
   signIn: '/sign-in',
   signUp: '/sign-up',
