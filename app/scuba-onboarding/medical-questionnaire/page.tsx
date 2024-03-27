b7c15bfce9ca0bf794e0f26d2016ffcfc07ff4ff
romane van brouhkoven <romane.to.code@gmail.com>
2024-03-27 14:07:32 +0000

    controller

--- app/scuba-onboarding/medical-questionnaire/page.tsx
+++ app/scuba-onboarding/medical-questionnaire/page.tsx
@@ -25,10 +25,9 @@ type MedicalQuestionnaireSchema = z.infer<typeof medicalQuestionnaireSchema>
 
 const MedicalQuestionnaire = () => {
   const router = useRouter()
-  const { setValue, control, handleSubmit } =
-    useForm<MedicalQuestionnaireSchema>({
-      resolver: zodResolver(medicalQuestionnaireSchema),
-    })
+  const { control, handleSubmit } = useForm<MedicalQuestionnaireSchema>({
+    resolver: zodResolver(medicalQuestionnaireSchema),
+  })
 
   const onSubmit = (data: MedicalQuestionnaireSchema) => {
     console.log(data)
@@ -61,52 +60,54 @@ const MedicalQuestionnaire = () => {
               )
             }}
           />
-          {/* <Checkbox.Root
-            defaultChecked={false}
-            onCheckedChange={(checked) => {
-              if (typeof checked !== 'boolean') return false
-
-              setValue('1', checked)
-            }}
-          >
-            <Checkbox.Indicator>
-              <Checkbox.Check />
-            </Checkbox.Indicator>
-          </Checkbox.Root> */}
           <Text className="max-w-[300px]">
             I have had problems with my lungs, breathing, heart and/or blood
             affecting my normal physical or mental performance
           </Text>
         </div>
         <div className="flex flex-row gap-2">
-          <Checkbox.Root
-            defaultChecked={false}
-            onCheckedChange={(checked) => {
-              if (typeof checked !== 'boolean') return false
-
-              setValue('2', checked)
+          <Controller
+            control={control}
+            name="2"
+            rules={{ required: 'This field is required' }}
+            render={({ field }) => {
+              return (
+                <Checkbox.Root
+                  defaultChecked={false}
+                  onCheckedChange={(checked) => {
+                    field.onChange(checked)
+                  }}
+                >
+                  <Checkbox.Indicator>
+                    <Checkbox.Check />
+                  </Checkbox.Indicator>
+                </Checkbox.Root>
+              )
             }}
-          >
-            <Checkbox.Indicator>
-              <Checkbox.Check />
-            </Checkbox.Indicator>
-          </Checkbox.Root>
+          />
           <Text className="max-w-[300px]">I am over 45 years of age.</Text>
         </div>
 
         <div className="flex flex-row gap-2">
-          <Checkbox.Root
-            defaultChecked={false}
-            onCheckedChange={(checked) => {
-              if (typeof checked !== 'boolean') return false
-
-              setValue('3', checked)
+          <Controller
+            control={control}
+            name="3"
+            rules={{ required: 'This field is required' }}
+            render={({ field }) => {
+              return (
+                <Checkbox.Root
+                  defaultChecked={false}
+                  onCheckedChange={(checked) => {
+                    field.onChange(checked)
+                  }}
+                >
+                  <Checkbox.Indicator>
+                    <Checkbox.Check />
+                  </Checkbox.Indicator>
+                </Checkbox.Root>
+              )
             }}
-          >
-            <Checkbox.Indicator>
-              <Checkbox.Check />
-            </Checkbox.Indicator>
-          </Checkbox.Root>
+          />
           <Text className="max-w-[300px]">
             I struggle to perform moderate exercise (for example, walk 1.6
             kilometer/one mile in 14 minutes or swim 200 meters/yards without
@@ -115,53 +116,74 @@ const MedicalQuestionnaire = () => {
           </Text>
         </div>
         <div className="flex flex-row gap-2">
-          <Checkbox.Root
-            defaultChecked={false}
-            onCheckedChange={(checked) => {
-              if (typeof checked !== 'boolean') return false
-
-              setValue('4', checked)
+          <Controller
+            control={control}
+            name="4"
+            rules={{ required: 'This field is required' }}
+            render={({ field }) => {
+              return (
+                <Checkbox.Root
+                  defaultChecked={false}
+                  onCheckedChange={(checked) => {
+                    field.onChange(checked)
+                  }}
+                >
+                  <Checkbox.Indicator>
+                    <Checkbox.Check />
+                  </Checkbox.Indicator>
+                </Checkbox.Root>
+              )
             }}
-          >
-            <Checkbox.Indicator>
-              <Checkbox.Check />
-            </Checkbox.Indicator>
-          </Checkbox.Root>
+          />
           <Text className="max-w-[300px]">
             I have had problems with my eyes, ears, or nasal passages/sinuses
           </Text>
         </div>
         <div className="flex flex-row gap-2">
-          <Checkbox.Root
-            defaultChecked={false}
-            onCheckedChange={(checked) => {
-              if (typeof checked !== 'boolean') return false
-
-              setValue('5', checked)
+          <Controller
+            control={control}
+            name="5"
+            rules={{ required: 'This field is required' }}
+            render={({ field }) => {
+              return (
+                <Checkbox.Root
+                  defaultChecked={false}
+                  onCheckedChange={(checked) => {
+                    field.onChange(checked)
+                  }}
+                >
+                  <Checkbox.Indicator>
+                    <Checkbox.Check />
+                  </Checkbox.Indicator>
+                </Checkbox.Root>
+              )
             }}
-          >
-            <Checkbox.Indicator>
-              <Checkbox.Check />
-            </Checkbox.Indicator>
-          </Checkbox.Root>
+          />
           <Text className="max-w-[300px]">
             I have had surgery within the last 12 months, OR I have ongoing
             problems related to past surgery.
           </Text>
         </div>
         <div className="flex flex-row gap-2">
-          <Checkbox.Root
-            defaultChecked={false}
-            onCheckedChange={(checked) => {
-              if (typeof checked !== 'boolean') return false
-
-              setValue('6', checked)
+          <Controller
+            control={control}
+            name="6"
+            rules={{ required: 'This field is required' }}
+            render={({ field }) => {
+              return (
+                <Checkbox.Root
+                  defaultChecked={false}
+                  onCheckedChange={(checked) => {
+                    field.onChange(checked)
+                  }}
+                >
+                  <Checkbox.Indicator>
+                    <Checkbox.Check />
+                  </Checkbox.Indicator>
+                </Checkbox.Root>
+              )
             }}
-          >
-            <Checkbox.Indicator>
-              <Checkbox.Check />
-            </Checkbox.Indicator>
-          </Checkbox.Root>
+          />
           <Text className="max-w-[300px]">
             I have lost consciousness, had migraine headaches, seizures, stroke,
             significant head injury, or suffer from persistent neurologic injury
@@ -169,18 +191,25 @@ const MedicalQuestionnaire = () => {
           </Text>
         </div>
         <div className="flex flex-row gap-2">
-          <Checkbox.Root
-            defaultChecked={false}
-            onCheckedChange={(checked) => {
-              if (typeof checked !== 'boolean') return false
-
-              setValue('7', checked)
+          <Controller
+            control={control}
+            name="7"
+            rules={{ required: 'This field is required' }}
+            render={({ field }) => {
+              return (
+                <Checkbox.Root
+                  defaultChecked={false}
+                  onCheckedChange={(checked) => {
+                    field.onChange(checked)
+                  }}
+                >
+                  <Checkbox.Indicator>
+                    <Checkbox.Check />
+                  </Checkbox.Indicator>
+                </Checkbox.Root>
+              )
             }}
-          >
-            <Checkbox.Indicator>
-              <Checkbox.Check />
-            </Checkbox.Indicator>
-          </Checkbox.Root>
+          />
           <Text className="max-w-[300px]">
             I am currently undergoing treatment (or have required treatment
             within the last five years) for psychological problems, personality
@@ -189,52 +218,73 @@ const MedicalQuestionnaire = () => {
           </Text>
         </div>
         <div className="flex flex-row gap-2">
-          <Checkbox.Root
-            defaultChecked={false}
-            onCheckedChange={(checked) => {
-              if (typeof checked !== 'boolean') return false
-
-              setValue('8', checked)
+          <Controller
+            control={control}
+            name="8"
+            rules={{ required: 'This field is required' }}
+            render={({ field }) => {
+              return (
+                <Checkbox.Root
+                  defaultChecked={false}
+                  onCheckedChange={(checked) => {
+                    field.onChange(checked)
+                  }}
+                >
+                  <Checkbox.Indicator>
+                    <Checkbox.Check />
+                  </Checkbox.Indicator>
+                </Checkbox.Root>
+              )
             }}
-          >
-            <Checkbox.Indicator>
-              <Checkbox.Check />
-            </Checkbox.Indicator>
-          </Checkbox.Root>
+          />
           <Text className="max-w-[300px]">
             I have had back problems, hernia, ulcers, or diabetes.
           </Text>
         </div>
         <div className="flex flex-row gap-2">
-          <Checkbox.Root
-            defaultChecked={false}
-            onCheckedChange={(checked) => {
-              if (typeof checked !== 'boolean') return false
-
-              setValue('9', checked)
+          <Controller
+            control={control}
+            name="9"
+            rules={{ required: 'This field is required' }}
+            render={({ field }) => {
+              return (
+                <Checkbox.Root
+                  defaultChecked={false}
+                  onCheckedChange={(checked) => {
+                    field.onChange(checked)
+                  }}
+                >
+                  <Checkbox.Indicator>
+                    <Checkbox.Check />
+                  </Checkbox.Indicator>
+                </Checkbox.Root>
+              )
             }}
-          >
-            <Checkbox.Indicator>
-              <Checkbox.Check />
-            </Checkbox.Indicator>
-          </Checkbox.Root>
+          />
           <Text className="max-w-[300px]">
             I have had stomach or intestine problems, including recent diarrhea
           </Text>
         </div>
         <div className="flex flex-row gap-2">
-          <Checkbox.Root
-            defaultChecked={false}
-            onCheckedChange={(checked) => {
-              if (typeof checked !== 'boolean') return false
-
-              setValue('10', checked)
+          <Controller
+            control={control}
+            name="10"
+            rules={{ required: 'This field is required' }}
+            render={({ field }) => {
+              return (
+                <Checkbox.Root
+                  defaultChecked={false}
+                  onCheckedChange={(checked) => {
+                    field.onChange(checked)
+                  }}
+                >
+                  <Checkbox.Indicator>
+                    <Checkbox.Check />
+                  </Checkbox.Indicator>
+                </Checkbox.Root>
+              )
             }}
-          >
-            <Checkbox.Indicator>
-              <Checkbox.Check />
-            </Checkbox.Indicator>
-          </Checkbox.Root>
+          />
           <Text className="max-w-[300px]">
             I am taking prescription medications (with the exception of birth
             control or anti-malarial drugs other than mefloquine (Lariam)).
